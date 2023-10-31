56683823176095a3a63b3d1518d701d57e885ac4
Rahul Annae <annaerahul@gmail.com>
2023-10-31 13:12:48 -0500

    build commit

--- portfolio/projects/base/src/app/app.component.ts
+++ portfolio/projects/base/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base';
+  counter = 3;
+  timer: any;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  @HostListener('click')
+  onKeyPress() {
+    this.counter = 3;
+  }
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      console.log('COUNTER', this.counter);
+      this.counter--;
+      if (this.counter === 0) {
+        this.httpClient.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(x => console.log(x));
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
 }
